7ff9e190b7b74311d5b286fdf5bf66ed7eb012df
samarthsarkar <samarthsarkar30@gmail.com>
2024-04-02 23:25:06 +0530

    Added Genpact Intern details
    Added a section for internships in the website as well as added Genpact as the first internship

--- src/components/Internship.js
+++ src/components/Internship.js
@@ -1,7 +1,7 @@
 import React from "react";
-// import { motion } from "framer-motion";
-// import { fadeIn } from "../variants";
-// import Img1 from "../assets/genpact-logo.png";
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
+import Img1 from "../assets/genlogo.jpg";
 const Internship = () => {
   return (
     <section className="section" id="internship">
@@ -11,7 +11,7 @@ const Internship = () => {
           <div className="flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0">
             {/* left aligned */}
             <div>
-              <h2 className="h2 leading-tight text-accent">
+              <h2 className="h2 mt-52 text-accent">
                 Internships
               </h2>
               <p className="max-w-sm mb-10">
@@ -19,35 +19,35 @@ const Internship = () => {
               </p>
             </div>
             {/* image */}
-            {/* <motion.div
-              onClick={() => {
-                window.open("https://github.com/samarthsarkar/VibeSync");
-              }}
-              variants={fadeIn("right", 0.2)}
-              initial="hidden"
-              whileInView={"show"}
-              viewport={{ once: false, amount: 0.3 }}
-              className="group relative overflow-hidden border-2 border-white/50 rounded-xl hover:cursor-pointer"
-            >
-              {/* overlay */}
-              {/* <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-              {/* img */}
-              {/* <img
-                className="group-hover:scale-125 transition-all duration-500"
-                src={Img1}
-                alt=""
-              />
-              {/* pretitle */}
-              {/* <div className="absolute -bottom-full left-1/2 group-hover:bottom-32 transition-all duration-500 z-50">
-                <span className="text-gradient text-3xl">VibeSync</span>
-              </div>
-              {/* title */}
-              {/* <div className="absolute -bottom-full left-1/2 group-hover:bottom-14 transition-all duration-700 z-50">
-                <span className="text-lg text-white">
-                  Music Player synced to spotify, API-data retrieval demonstration
-                </span>
-              </div>
-            </motion.div> */}
+            <div className="flex gap-x-4">
+                <motion.div
+                variants={fadeIn("right", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="group relative overflow-hidden border-2 border-white/50 rounded-xl hover:cursor-pointer w-64 h-64"
+                >
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-10 h-10 absolute z-40 transition-all duration-300"></div>
+                {/* img */}
+                <img
+                    className="group-hover:scale-125 transition-all duration-500"
+                    src={Img1}
+                    alt=""
+                />
+                </motion.div>
+                <div className="flex flex-col justify-center">
+                    <h2 className="genp" style={{ fontSize: "40px" }}>
+                        Genpact
+                    </h2>
+                    <p>
+                        6-Month internship at Genpact India, located in Bengaluru, as an ML engineering intern.<br></br>
+                        I worked on end-to-end projects and conducted data exploration, exploratory data analysis,<br></br>
+                        feature engineering, model training and deployment.
+
+                    </p>
+                </div>
+            </div>
           </div>
         </div>
       </div>
