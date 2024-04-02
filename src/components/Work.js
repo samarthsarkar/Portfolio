6cb47d2212464a0eedec19c12ace004b85c01910
samarthsarkar <samarthsarkar30@gmail.com>
2024-04-02 20:55:16 +0530

    internships

--- src/components/Work.js
+++ src/components/Work.js
@@ -8,7 +8,9 @@ import Img3 from "../assets/portfolio-img3.png";
 const Work = () => {
   return (
     <section className="section" id="work">
-      <h2>tester text</h2>
+      <h2 className="h2 leading-tight text-accent">
+        Internships
+      </h2>
       <div className="container mt-52 mx-auto">
       {/* inside project class */}
         <div className="flex flex-col lg:flex-row gap-x-10">
