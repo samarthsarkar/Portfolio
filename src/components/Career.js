d78b8a4c5731b343c0088fbc023492a122a289fa
samarthsarkar <samarthsarkar30@gmail.com>
2025-07-22 17:30:42 +0530

    Added experience - Itron

--- src/components/Career.js
+++ src/components/Career.js
@@ -2,6 +2,7 @@ import React from "react";
 import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 import Img1 from "../assets/genlogo.jpg";
+import Img2 from "../assets/itron.png";
 const Career = () => {
   return (
     <section className="section" id="internship">
@@ -14,10 +15,42 @@ const Career = () => {
               <h2 className="h2 mt-52 text-accent">
                 Professional Career
               </h2>
-              <p className="max-w-sm mb-10">
+              <h3 className="max-w-sm mb-10">
                 A glimpse into my professional growth.
-              </p>
+              </h3>
             </div>
+            {/* image */}
+            <div className="flex gap-x-4">
+                <motion.div
+                variants={fadeIn("right", 0.2)}
+                initial="hidden"
+                whileInView={"show"}
+                viewport={{ once: false, amount: 0.3 }}
+                className="group relative overflow-hidden border-2 border-white/50 rounded-xl hover:cursor-pointer w-64 h-64"
+                >
+                {/* overlay */}
+                <div className="group-hover:bg-black/70 w-10 h-10 absolute z-40 transition-all duration-300"></div>
+                {/* img */}
+                <img
+                    className="group-hover:scale-125 transition-all duration-500"
+                    src={Img2}
+                    alt=""
+                />
+                </motion.div>
+                <div className="flex flex-col justify-center">
+                    <h2 className="itr" style={{ fontSize: "40px" }}>
+                        Itron
+                    </h2>
+                    <p>
+                        6-Month internship at Genpact India, located in Bengaluru, as an ML engineering intern.<br></br>
+                        I worked on end-to-end projects and conducted data exploration, exploratory data analysis,<br></br>
+                        feature engineering, model training and deployment.
+
+                    </p>
+                </div>
+            </div>
+
+
             {/* image */}
             <div className="flex gap-x-4">
                 <motion.div
